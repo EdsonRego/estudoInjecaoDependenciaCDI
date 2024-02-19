74795d97939013fbd16026cdf067ab2f1f346365
EdsonRego <edsonxrego@gmail.com>
2024-02-18 23:25:54 -0300

    Spring Project Created

---
+++ src/test/java/com/edsonrego/estudoInjecaoDependenciaCDI/EstudoInjecaoDependenciaCdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsonrego.estudoInjecaoDependenciaCDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoInjecaoDependenciaCdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
