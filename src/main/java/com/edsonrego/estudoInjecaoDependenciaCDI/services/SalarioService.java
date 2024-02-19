18195a5d19729b83426c5903c1715afc7453ee1c
EdsonRego <edsonxrego@gmail.com>
2024-02-18 23:52:30 -0300

    Spring Component Injection - using an Annotation

--- src/main/java/com/edsonrego/estudoInjecaoDependenciaCDI/services/SalarioService.java
+++ src/main/java/com/edsonrego/estudoInjecaoDependenciaCDI/services/SalarioService.java
@@ -1,15 +1,13 @@
 package com.edsonrego.estudoInjecaoDependenciaCDI.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service //ou @Component
 public class SalarioService {
 
+    @Autowired
     private PrevidenciaService previdenciaService;
-
-    public SalarioService(PrevidenciaService previdenciaService) {
-        this.previdenciaService = previdenciaService;
-    }
 
     public double salarioLiquido(double salarioBruto){
         double imposto = salarioBruto * 0.2;
